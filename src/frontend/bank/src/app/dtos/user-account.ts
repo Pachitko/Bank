a500e53833ad1a8b2ff3f199a96edf56a99a41bd
Pachitko <pachitko@mail.ru>
2022-09-09 21:47:45 +0300

    feat(user): add accounts property

---
+++ src/frontend/bank/src/app/dtos/user-account.ts
@@ -0,0 +1,4 @@
+export class UserAccount {
+    currency: string | undefined;
+    money: number | undefined;
+}
--- src/frontend/bank/src/app/dtos/user.ts
+++ src/frontend/bank/src/app/dtos/user.ts
@@ -1,4 +1,7 @@
+import { UserAccount } from "./user-account";
+
 export class User {
     username: string = "";
     email: string = "";
+    accounts: UserAccount[] = [];
 }
